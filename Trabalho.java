c5788e5fb9ecad1bf078cc63f126fd8de8e70e06
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-04-20 15:44:53 -0300

    Adição de Classe "Cliente"

--- Trabalho.java
+++ Trabalho.java
@@ -21,3 +21,27 @@ class Conta {
         return saldo;
     }
 }
+
+class Cliente extends Thread {
+    private static final int[] VALORES_COMPRA = {100, 200};
+    private Conta conta;
+
+    public Cliente(Conta conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int valorCompra = VALORES_COMPRA[ThreadLocalRandom.current().nextInt(VALORES_COMPRA.length)];
+            synchronized (conta) {
+                if (conta.getSaldo() >= valorCompra) {
+                    conta.sacar(valorCompra);
+                    System.out.println(Thread.currentThread().getName() + " realizou uma compra de R$ " + valorCompra);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
