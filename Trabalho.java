2cca6fb9ad9bcca2dce00c370e5b7dee03496614
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-04-20 17:44:24 -0300

    Adição de Classe "Funcionário"

--- Trabalho.java
+++ Trabalho.java
@@ -45,3 +45,25 @@ class Cliente extends Thread {
         }
     }
 }
+
+class Funcionario extends Thread {
+    private static final double SALARIO = 1400;
+    private Conta contaSalario;
+    private Conta contaInvestimento;
+
+    public Funcionario(Conta contaSalario, Conta contaInvestimento) {
+        this.contaSalario = contaSalario;
+        this.contaInvestimento = contaInvestimento;
+    }
+
+    @Override
+    public void run() {
+        synchronized (contaSalario) {
+            contaSalario.depositar(SALARIO);
+            double valorInvestimento = SALARIO * 0.2;
+            contaInvestimento.depositar(valorInvestimento);
+            System.out.println(Thread.currentThread().getName() + " recebeu o salário de R$ " + SALARIO +
+                    " e investiu R$ " + valorInvestimento);
+        }
+    }
+}
