c39c1dffdfa28b1e4b116c8f4561e3327d222495
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-04-23 09:10:14 -0300

    Adição de Classe "Loja"

--- Trabalho.java
+++ Trabalho.java
@@ -67,3 +67,18 @@ class Funcionario extends Thread {
         }
     }
 }
+
+class Loja {
+    private Conta conta;
+
+    public Loja(Conta conta) {
+        this.conta = conta;
+    }
+
+    public synchronized void pagarSalarios() {
+        double totalSalarios = 0;
+        // Calcular o total dos salários dos funcionários
+        // Pagar os salários
+        System.out.println("Loja pagou R$ " + totalSalarios + " em salários");
+    }
+}
